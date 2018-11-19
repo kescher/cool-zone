497e78ccae25ebffa6298db92f4d73db06055a79
Kristen Escher <kescher@umich.edu>
2018-11-18 23:02:11 -0500

    Updates

---
+++ precache-manifest.a9b4bc6b221e66dc1d2a7f1afd8d155a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cool-zone/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a2ff4ecd3a5643d6e88b",
+    "url": "/cool-zone/static/js/runtime~main.a2ff4ecd.js"
+  },
+  {
+    "revision": "b6b940a287c33ba764d9",
+    "url": "/cool-zone/static/js/main.b6b940a2.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/cool-zone/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "b6b940a287c33ba764d9",
+    "url": "/cool-zone/static/css/main.8a041956.chunk.css"
+  },
+  {
+    "revision": "3677fd84206ba2c65b4fd952d5f2f2ce",
+    "url": "/cool-zone/index.html"
+  }
+];
